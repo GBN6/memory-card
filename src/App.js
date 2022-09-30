abe81201b7d18a6b0a92934e3e17dc0a0c28ecaa
GBN6 <czesiox8@gmail.com>
2022-09-30 16:47:15 +0200

    Create header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+const App = (props) => {
+
   return (
     <div className="App">
-      hello world
+      <Header />
     </div>
   );
 }
