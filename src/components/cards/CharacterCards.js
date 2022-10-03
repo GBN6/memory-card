a24258aad86a63c2fca2a9e2b32c86291c12a7f6
GBN6 <czesiox8@gmail.com>
2022-10-03 14:17:04 +0200

    Add character cards

---
+++ src/components/cards/CharacterCards.js
@@ -0,0 +1,65 @@
+import goku from '../../assets/characters/goku.png';
+import vegeta from '../../assets/characters/vegeta.jpeg';
+import gohan from '../../assets/characters/gohan.png';
+import trunks from '../../assets/characters/trunks.jpeg';
+import brolly from '../../assets/characters/brolly.png';
+import frieza from '../../assets/characters/frieza.png';
+import krillan from '../../assets/characters/krillan.png';
+import nappa from '../../assets/characters/nappa.png';
+import pan from '../../assets/characters/pan.png';
+import piccolo from '../../assets/characters/piccolo.png';
+import raditz from '../../assets/characters/raditz.png';
+import tien from '../../assets/characters/tien.png';
+
+const charactersCards = [
+  {
+    src: goku,
+    name: 'Goku',
+  },
+  {
+    src: vegeta,
+    name: 'Vegeta',
+  },
+  {
+    src: gohan,
+    name: 'Gohan',
+  },
+  {
+    src: trunks,
+    name: 'Trunks',
+  },
+  {
+    src: brolly,
+    name: 'Brolly',
+  },
+  {
+    src: frieza,
+    name: 'Frieza',
+  },
+  {
+    src: krillan,
+    name: 'Krillan',
+  },
+  {
+    src: pan,
+    name: 'Pan',
+  },
+  {
+    src: piccolo,
+    name: 'Piccolo',
+  },
+  {
+    src: nappa,
+    name: 'Nappa',
+  },
+  {
+    src: raditz,
+    name: 'Raditz',
+  },
+  {
+    src: tien,
+    name: 'Tien',
+  },
+];
+
+export default charactersCards;
